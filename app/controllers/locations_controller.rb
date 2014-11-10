5d460278f4eca265b416114b48eb1e9a85016e80
Vitaliy <vitaliyb872@gmail.com>
2014-11-10 17:19:59 +0100

    addError404

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,10 @@ class LocationsController < ApplicationController
 	def index
 		@locations = Location.last_created(10)
 	end
+	def show
+		@location = Location.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+ 		render status: 404, 
+			file: "#{Rails.root}/public/404"
+	end
 end
